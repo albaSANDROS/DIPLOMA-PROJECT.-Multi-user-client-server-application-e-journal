55dc819096e92624c150b2b40b86d64f26407a82
Aliaksandr Basko <baskoaleksandr@gmail.com>
2022-02-10 17:28:40 +0300

    Started work with QTcreator. Connected to DB

---
+++ QTcreator/Diary/connection.h
@@ -0,0 +1,22 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#include <QMessageBox>
+#include <QtSql/QSqlDatabase>
+
+inline bool createConnection(){
+    QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
+    db.setDatabaseName("DiaryDB");
+    db.setUserName("postgres");
+    db.setPassword("1");
+    if (!db.open()){
+        QMessageBox::warning(0,"Ошибка БД", "Something Wrong");
+        return false;
+    }
+    else{
+        QMessageBox::information(0, "Успешно", "Соединение с БД установлено!");
+        return true;
+    }
+}
+
+#endif // CONNECTION_H
