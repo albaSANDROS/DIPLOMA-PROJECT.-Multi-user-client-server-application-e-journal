e6963b2636e87a8103ce94e1eb83919df2318d66
Aliaksandr Basko <baskoaleksandr@gmail.com>
2022-02-11 20:29:20 +0300

    Added authorization window

--- QTcreator/Diary/mainwindow.cpp
+++ QTcreator/Diary/mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QMessageBox>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,5 +11,17 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_auth_confirm_clicked()
+{
+    QString auth_login = ui -> auth_login -> text();
+    QString auth_pass  = ui -> auth_pass  -> text();
+
+    if(auth_login == "" && auth_pass == ""){
+        QMessageBox::information(this, "Authorization", "Authorization confirmed");
+    }else
+        QMessageBox::warning(this, "Authorization", "Authorization denied");
 }
 
--- QTcreator/build-Diary-Desktop_Qt_6_3_0_MinGW_64_bit-Debug/ui_mainwindow.h
+++ QTcreator/build-Diary-Desktop_Qt_6_3_0_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -11,9 +11,15 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,6 +28,17 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QGroupBox *groupBox;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout_3;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout;
+    QLabel *label;
+    QLabel *label_2;
+    QVBoxLayout *verticalLayout_2;
+    QLineEdit *auth_login;
+    QLineEdit *auth_pass;
+    QPushButton *auth_confirm;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -29,12 +46,62 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(410, 210);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        groupBox = new QGroupBox(centralwidget);
+        groupBox->setObjectName(QString::fromUtf8("groupBox"));
+        groupBox->setGeometry(QRect(50, 10, 311, 151));
+        widget = new QWidget(groupBox);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(30, 30, 271, 101));
+        verticalLayout_3 = new QVBoxLayout(widget);
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        verticalLayout_3->setContentsMargins(0, 0, 0, 0);
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        label = new QLabel(widget);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        verticalLayout->addWidget(label);
+
+        label_2 = new QLabel(widget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        verticalLayout->addWidget(label_2);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        auth_login = new QLineEdit(widget);
+        auth_login->setObjectName(QString::fromUtf8("auth_login"));
+
+        verticalLayout_2->addWidget(auth_login);
+
+        auth_pass = new QLineEdit(widget);
+        auth_pass->setObjectName(QString::fromUtf8("auth_pass"));
+
+        verticalLayout_2->addWidget(auth_pass);
+
+
+        horizontalLayout->addLayout(verticalLayout_2);
+
+
+        verticalLayout_3->addLayout(horizontalLayout);
+
+        auth_confirm = new QPushButton(widget);
+        auth_confirm->setObjectName(QString::fromUtf8("auth_confirm"));
+
+        verticalLayout_3->addWidget(auth_confirm);
+
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 410, 22));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -48,6 +115,10 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        groupBox->setTitle(QCoreApplication::translate("MainWindow", "Authorization", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Login", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Password", nullptr));
+        auth_confirm->setText(QCoreApplication::translate("MainWindow", "Confirm", nullptr));
     } // retranslateUi
 
 };
